6d6370090ad6ea75cc3212cf94deaf66c6d136e4
Sause <lazureys@gmail.com>
2016-11-16 15:48:13 +0800

    first commit

---
+++ todo-list/src/test/java/com/tw/bootcamp/controllers/GreetingControllerTest.java
@@ -0,0 +1,28 @@
+package com.tw.bootcamp.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GreetingController.class)
+public class GreetingControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_hello_world_by_default() throws Exception {
+        mockMvc.perform(get("/").contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", is("Hello, World")));
+    }
+}
